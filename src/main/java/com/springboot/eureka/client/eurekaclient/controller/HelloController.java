36f02050cd043bc040daf789d80b703b58c7ccab
kuailexiaoyi <409718341@qq.com>
2019-06-01 10:50:24 +0800

    SpringCloud Eureka-Client 服务发现

---
+++ src/main/java/com/springboot/eureka/client/eurekaclient/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.springboot.eureka.client.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description:
+ * @Auther: zrblog
+ * @CreateTime: 2019-06-01 10:30
+ * @Version:v1.0
+ */
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    @Value("${server.port}")
+    private String port;
+
+    public String sayHello() {
+        return "Hello World,I'm from port:" + port;
+    }
+}
